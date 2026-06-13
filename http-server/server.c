f66d8342ce6788a38fa3fae3ead9ac6f08875b68
KiriTrip <eggor01042010@gmail.com>
2026-06-13 21:30:06 +0300

    Server need Windows

--- http-server/server.c
+++ http-server/server.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <stdlib.h>
 
 int main()
 {
-    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        perror("Socket error");
+        exit(-1);
+    }
+    struct sockaddr addr;
+    int bind_result = bind(sockfd, &addr, sizeof(struct sockaddr));
+
+    printf("Bind result is: %d\n", bind_result);
+
     while (1)
     {
         // Some kind of request must be received here
